f19b194d6305ca98671bd6c84b139f81e09fe303
RommelMk <rommelgmk@gmail.com>
2022-08-24 18:40:03 -0300

    Clase Javascript

---
+++ Clase Js/clase.js
@@ -0,0 +1,19 @@
+/*
+let nombre = prompt("Ingrese tu nombre");
+let apellido = prompt("Introduce tu apellido");
+let edad = prompt("Introduce tu edad");
+let genero = prompt("Introduce tu genero");
+let formulario = nombre + " " + apellido + " edad= " + edad + " genero= " + genero;
+alert(formulario);
+
+console.log(formulario);
+*/
+
+
+let numero = parseInt(prompt("Ingresa el código de 3 digitos"));
+
+if(numero > 999) {
+    alert ("código incorrecto");
+}else {
+    alert ("código aceptado");
+}
